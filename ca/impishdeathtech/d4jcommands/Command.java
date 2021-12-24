d63921cbd3b9f50dc82428e6516c70d6e263895e
Christopher Stephen Rafuse <81781310+ImpishDeathTech@users.noreply.github.com>
2021-12-24 17:38:49 +0000

    Update Command.java

--- ca/impishdeathtech/d4jcommands/Command.java
+++ ca/impishdeathtech/d4jcommands/Command.java
@@ -2,7 +2,7 @@
   haven't really done much to this
 */
 
-package d4jcommands;
+package ca.impishdeathtech.d4jcommands;
 
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import reactor.core.publisher.Mono;
